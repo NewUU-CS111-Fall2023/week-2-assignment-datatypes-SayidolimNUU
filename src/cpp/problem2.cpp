8e26d28139d64a139af76c4e33cd49cfb10f00d0
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2023-10-25 17:54:25 +0500

    Update problem2.cpp

--- src/cpp/problem2.cpp
+++ src/cpp/problem2.cpp
@@ -2,3 +2,26 @@
  * Author: Sayidolimxon Usmanxonov
  * Date: 25.10.2023
  */
+
+#include <iostream>
+#include <time.h>
+
+using std::cin, std::cout, std::string, std::endl;
+
+int main() {
+    srand(time(0));
+    int input = 0, randomNumber = rand() % 99 + 1;
+    cout << "Guess my random from 1 to 100" << endl;
+    while (input != randomNumber) {
+        cin >> input;
+        if (input > randomNumber) {
+            cout << "too high" << endl;
+        }
+        else if (input < randomNumber) {
+            cout << "too low" << endl;
+        }
+        else {
+            cout << "game over" << endl;
+        }
+    }
+}
