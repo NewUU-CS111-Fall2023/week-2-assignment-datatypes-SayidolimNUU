8ceccc06de51bf9ae7c9058d3d2a1972e7b57c87
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2023-10-25 17:39:27 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,21 +1,10 @@
 /*
- * Author:
- * Date:
+ * Author: Sayidolimxon Usmanxonov
+ * Date: 25.10.2023
  */
 
 #include <iostream>
-#include "task_1.h"
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
     return 0;
 }
