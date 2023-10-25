fcc04a182af7b8373c6ddb63c543222be9fa5f62
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2023-10-25 21:44:29 +0500

    Update task1.h

--- src/cpp/task1.h
+++ src/cpp/task1.h
@@ -9,5 +9,5 @@ using std::cin, std::cout, std::string, std::endl;
 
 string task1(int input) {
     return (input % 400 == 0 || (input % 4 == 0 && input % 100 != 0)) ? "It is a leap year"
-    : "It is NOT a leap year";
+                                                                      : "It is NOT a leap year";
 }
